1e3827d594daa43b5f6dc2c314aee33b897068ac
Eric Oliveira <ericjmoliveira@gmail.com>
2024-11-26 14:21:27 -0300

    chore: add initial swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Project Management')
+    .setDescription('Project Management REST API')
+    .setVersion('1.0')
+    .addTag('project')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
